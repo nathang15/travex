cd02b177e23b40010aee8979fedb19d7c15e5779
nathang15 <nhanngyn15@gmail.com>
2023-04-01 18:03:24 -0400

    commit index in api

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
 import axios from "axios";
-
-
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-export const getPlacesData = async (ne, sw) => {
+export const getPlacesData = async (sw, ne) => {
     try {
-        const response = await axios.get(URL, {
-            params: {
-                bl_latitude: sw.lat,
-                tr_latitude: ne.lat,
-                bl_longitude: sw.lng,
-                tr_longitude: ne.lng,
-            },
-            headers: {
-                'X-RapidAPI-Key': process.env.REACT_APP_TRAVEL_ADVISOR_KEY,
-                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-            }
-        })
-        return response.data
-    } catch (err) {
-        console.log(err)
+        const { data: {data} } = await axios.get(URL, {
+          
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': 'f6ae5a8bb5mshd7225698491358cp130c20jsne6c9a0f5b1fb',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
+
+        return data
+    }
+
+    catch (error) {
+        console.log(error)
     }
 }
