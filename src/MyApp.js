9df85238f5f123506ee7fb8454546dc8860c01f1
nathang15 <nhanngyn15@gmail.com>
2023-04-01 01:45:51 -0400

    Modifies MyApp and miscellaneous

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from './components/Header/Header';
+import List from './components/List/List';
+import Helmet from 'react-helmet';
+
 
 export default function MyApp() {
     return (
       <div className="">
+      <Helmet bodyAttributes={{style: 'background-color : #F3F4F6'}}/>
       <Header />
+
+      <main className="flex">
+        <List />
+      </main>
     </div>
       
     )
