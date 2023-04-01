0870c83c493dd472ed5e3bea4c1e5638482eab4a
nathang15 <nhanngyn15@gmail.com>
2023-03-31 22:52:28 -0400

    add tailwind css and create folders for frontend

---
+++ src/MyApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from './components/Header/Header';
+
+export default function MyApp() {
+    return (
+      <div>
+        <Header/>
+      </div>
+      
+    )
+  }
